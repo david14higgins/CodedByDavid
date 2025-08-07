b0bbf7b7720858497b655335d513febae669d472
David Higgins <david14higgins@gmail.com>
2025-08-07 15:40:34 +0100

    start about page

--- src/App.jsx
+++ src/App.jsx
@@ -24,7 +24,20 @@ function App() {
           </div>
         </div>
       </div>
-      
+      <div className="about">
+        <div className="about-content">
+          <div className="left-content">
+            <h1>David Higgins</h1>
+            <h2>Junior Software Engineer</h2>
+            <p>I’m a passionate software developer with a strong foundation in computer science and a focus on building efficient, scalable, and user-friendly applications. I enjoy solving complex problems through clean code and thoughtful design, whether working on the frontend, backend, or full-stack systems. With experience across a range of technologies and languages, I continuously seek to learn and adapt in a fast-paced, ever-evolving industry. I'm driven by curiosity, collaboration, and the desire to create impactful digital solutions.</p>
+          </div>
+          <div className="right-content">
+            <div className="photo-container">
+              <img src="src/assets/images/graduation.jpg" alt="David Higgins" />
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   )
 }
