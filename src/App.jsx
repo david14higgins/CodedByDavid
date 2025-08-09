ad3e6e73a69608d0ef12bf134d27b45e8488e7e9
David Higgins <david14higgins@gmail.com>
2025-08-09 15:37:56 +0100

    navbar implemented

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import Home from './Home/Home';
 import About from './About/About';
+import Navbar from './Navbar/Navbar';
 
 function App() {
   return (
     <>
+      < Navbar />
       < Home />
       < About />
     </>
