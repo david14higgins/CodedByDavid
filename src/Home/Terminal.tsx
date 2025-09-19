ce43a4b397f55bd763a67dd9184c9f9d31c0a727
David Higgins <david.higgins@red-gate.com>
2025-09-19 14:45:42 +0100

    terminal pop in effect

--- src/Home/Terminal.tsx
+++ src/Home/Terminal.tsx
@@ -1,9 +1,18 @@
+import { useEffect, useState } from 'react';
 import CodeSnippet from './CodeSnippet'
 
 function Terminal() {
+  const [showTerminal, setShowTerminal] = useState(false);
+
+  useEffect(() => {
+    // Delay to trigger the pop-up animation after mount
+    const timer = setTimeout(() => setShowTerminal(true), 500); // 100ms delay
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
     <>
-      <div className="terminal">
+      <div className={`terminal${showTerminal ? " pop-in" : ""}`}>
         <div className="taskbar">
           <div className="taskbar-buttons">
             <span className="button red-button"></span>
