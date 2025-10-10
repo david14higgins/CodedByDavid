b9d469ce0287b8ac5fcfbdc36f48a22d1b2c1f68
David Higgins <david.higgins@red-gate.com>
2025-10-10 14:57:49 +0100

    move icons menu to navbar

--- src/Navbar/Navbar.tsx
+++ src/Navbar/Navbar.tsx
@@ -1,10 +1,12 @@
 import './Navbar.css';
 import { Element, Link } from "react-scroll";
+import Links from '../Links/Links';
 
 function Navbar() {
     return (
         <>
             <nav className="navbar">
+                <Links />
                 <ul>
                     <li><Link to="home-page" smooth={true} duration={1000}>Home</Link></li>
                     <li><Link to="about-page" smooth={true} duration={1000}>About</Link></li>
